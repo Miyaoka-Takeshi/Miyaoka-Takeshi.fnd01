77a38d0032b0274e6a6883b123aad35960e84d22
Miyaoka-Takeshi <takeshi_miyaoka@mail.toyota.co.jp>
2025-06-24 10:56:53 +0900

    Delete result.js

--- result.js
+++
@@ -1,8 +0,0 @@
-const container = document.getElementById('container');
-const data = JSON.parse(localStorage.getItem('tooltipData') || '{}');
-
-for (const key in data) {
-const div = document.createElement('div');  //div追加
-div.textContent = `${key}: ${data[key]}`;   //オブジェクト追加
-container.appendChild(div);                 //要素追加
-}
