aa000935ac969479ffce379458a61f8ab19e1e73
Miyaoka-Takeshi <takeshi_miyaoka@mail.toyota.co.jp>
2025-06-26 11:40:47 +0900

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -27,13 +27,6 @@ function createTile() { //タイル生成
   }
 }
 
-function swap(index, target) { //配列の入れ替え replaceは恒久より　swap一時的
-  let pending = tiles[index];
-  tiles[index] = tiles[target];
-  tiles[target] = pending;
-  createTile();           //タイル生成
-}
-
 function move(eventKey) {  //方向(event.key)
   const index = tiles.indexOf(0);  //0のインデックスは?
   const x = index % 3;                //X座標　壁判定のため
@@ -46,14 +39,75 @@ function move(eventKey) {  //方向(event.key)
     case "ArrowLeft": if (x < 2){ target = index + 1; break;} //→
   }
   if (target !== undefined){ swap(index, target);}  //配列[0-8]外でなければ実行
+  let count=0;
+  for(let i=0;i<9;i++){
+    if(JSON.stringify(tiles[i])===JSON.stringify(targetTiles[i])){
+      count++;
+    }
+    if(count>=9){
+      console.log("Game Clear");
+      gameClear();
+    }
+  }
 }
 
+function swap(index, target) { //配列の入れ替え replaceは恒久より　swap一時的
+  let pending = tiles[index];
+  tiles[index] = tiles[target];
+  tiles[target] = pending;
+  createTile();           //タイル生成
+}
+
+function change() {
+  randomNum = Math.floor(Math.random() * 6+1); //1-6
+  console.log(randomNum);
+  if(randomNum===1){
+    tiles = [3, 8, 5, 4, 7, 1, 2, 6, 0]; //初期値
+  }
+  else if(randomNum===2){
+    tiles = [7, 6, 8, 5, 4, 1, 3, 2, 0]; //初期値
+  }
+  else if(randomNum===3){
+    tiles = [3, 5, 7, 2, 8, 4, 1, 6, 0]; //初期値
+  }
+  else if(randomNum===4){
+    tiles = [7, 1, 5, 2, 4, 6, 8, 3, 0]; //初期値
+  }
+  else if(randomNum===5){
+    tiles = [5, 3, 1, 7, 2, 6, 4, 8, 0]; //初期値
+  }
+  else if(randomNum===6){
+    tiles = [4, 1, 3, 7, 2, 5, 8, 6, 0]; //初期値
+  }
+  createTile();
+}
+
+function gameClear(){
+  const element = document.createElement("P");
+  element.style.color = "red";
+  element.textContent = "Game Clear!!";
+  document.body.appendChild(element);
+}
+
+/*
+function Select() {
+  let select = document.querySelector("#select");
+  let size=select.value; // 選択中の値を取得
+  if(size==="3x3"){SIZE=3;}
+  else if(size==="4x4"){SIZE=4;}
+}
+*/
+
+//let SIZE;
+let randomNum;
+let tiles = [4, 1, 3, 7, 2, 5, 8, 6, 0]; //初期値
+
 const puzzle = document.getElementById("puzzle"); //divのidを検索
-//let tiles = [1, 2, 3, 4, 5, 6, 7, 8, 0]; //初期値
-let tiles = [4, 1, 3, 7, 2, 5, 0, 8, 6]; //初期値
+let targetTiles = [1, 2, 3, 4, 5, 6, 7, 8, 0]; //初期値
 
 document.addEventListener("keydown", (event) => { //function(event)
   move(event.key);
 });
 
+//document.addEventListener("click", (change));
 createTile();
